77729110b53496b25410f2bd8a9c681613bb3a71
unknown <hamaday468@gmail.com>
2025-04-27 17:54:02 +0300

    finish functional game

---
+++ pong.js
@@ -0,0 +1,128 @@
+//board
+let board;
+let boardWidth = 600;
+let boardHeight = 600;
+let context;
+
+let playerWidth = 10;
+let playerHeight = 50;
+let playerVelocityY = 0;
+
+let player1 = {
+  x: 10,
+  y: boardHeight / 2 - playerHeight / 2,
+  width: playerWidth,
+  height: playerHeight,
+  velocityY: playerVelocityY,
+  color: "blue",
+};
+
+let player2 = {
+  x: boardWidth - playerWidth - 10,
+  y: boardHeight / 2 - playerHeight / 2,
+  width: playerWidth,
+  height: playerHeight,
+  velocityY: playerVelocityY,
+  color: "blue",
+};
+
+let ballWidth = 10;
+let ballHeight = 10;
+let ball = {
+  x: boardWidth / 2,
+  y: boardHeight / 2,
+  width: ballWidth,
+  height: ballHeight,
+  velocityX: 1,
+  velocityY: 2,
+};
+
+window.onload = () => {
+  board = document.getElementById("board");
+  board.width = boardWidth;
+  board.height = boardHeight;
+  context = board.getContext("2d");
+
+  // draw player 1
+  context.fillStyle = player1.color;
+  context.fillRect(player1.x, player1.y, player1.width, player1.height);
+
+  //draw the ball
+
+  requestAnimationFrame(gameLoop);
+  document.addEventListener("keydown", movePlayer);
+  document.addEventListener("keyup", stopPlayer);
+};
+
+function gameLoop() {
+  requestAnimationFrame(gameLoop);
+  context.clearRect(0, 0, boardWidth, boardHeight);
+
+  context.fillStyle = player1.color;
+  player1.y += player1.velocityY;
+  context.fillRect(player1.x, player1.y, player1.width, player1.height);
+
+  context.fillStyle = player2.color;
+  player2.y += player2.velocityY;
+  context.fillRect(player2.x, player2.y, player2.width, player2.height);
+
+  // draw the ball
+  context.fillStyle = "white";
+  ball.x += ball.velocityX;
+  ball.y += ball.velocityY;
+  context.fillRect(ball.x, ball.y, ball.width, ball.height);
+
+  if (ball.y == 0 || ball.y + ball.height >= boardHeight) {
+    ball.velocityY = -ball.velocityY;
+  } else if (detectCollision(ball, player1) || detectCollision(ball, player2)) {
+    if (player2.x + player2.width > ball.x) {
+      ball.velocityX = -ball.velocityX;
+    } else if (player1.x < ball.x + ball.width) {
+      ball.velocityX = -ball.velocityX;
+    }
+  }
+}
+
+function movePlayer(e) {
+  if (e.key === "ArrowUp") {
+    player1.velocityY -= 1;
+  } else if (e.key === "ArrowDown") {
+    player1.velocityY += 1;
+  }
+
+  if (e.key === "w") {
+    player2.velocityY -= 1;
+  } else if (e.key === "s") {
+    player2.velocityY += 1;
+  }
+
+  // Prevent the player from going out of bounds
+  if (player1.y < 0) {
+    player1.y = 0;
+  } else if (player1.y + player1.height > boardHeight) {
+    player1.y = boardHeight - player1.height;
+  }
+
+  if (player2.y < 0) {
+    player2.y = 0;
+  } else if (player2.y + player2.height > boardHeight) {
+    player2.y = boardHeight - player2.height;
+  }
+}
+
+function stopPlayer(e) {
+  if (["ArrowUp", "ArrowDown"].includes(e.key)) {
+    player1.velocityY = 0;
+  } else if (["w", "s"].includes(e.key)) {
+    player2.velocityY = 0;
+  }
+}
+
+function detectCollision(a, b) {
+  return (
+    a.x < b.x + b.width &&
+    a.x + a.width > b.x &&
+    a.y < b.y + b.height &&
+    a.y + a.height > b.y
+  );
+}
